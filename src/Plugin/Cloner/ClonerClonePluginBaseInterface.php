9c761c5c0a6d62ccbd83667b9d4df1e753d93a04
Niklan <niklanrus@gmail.com>
2019-01-29 19:36:57 +0500

    Added example with using plugin manually.

--- src/Plugin/Cloner/ClonerClonePluginBaseInterface.php
+++ src/Plugin/Cloner/ClonerClonePluginBaseInterface.php
@@ -23,6 +23,6 @@ interface ClonerClonePluginBaseInterface extends PluginInspectionInterface {
    *   An array with context of clone. Can contain:
    *   - form_state: An instance of $form_state if called from form.
    */
-  public function cloneEntity(EntityInterface $entity_source, EntityInterface $entity_destination, array $context);
+  public function cloneEntity(EntityInterface $entity_source, EntityInterface $entity_destination, array $context = []);
 
 }
